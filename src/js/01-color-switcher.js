1042063484cf1deaa28aa696329a2b37c3d1dc58
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2023-04-09 18:14:05 +0300

    add task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+
+const refs = {
+   startBtn: document.querySelector('button[data-start]'),
+   stopBtn: document.querySelector('button[data-stop]')
+};
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick)
+
+const delayValue = 1000;
+let intervalId = null;
+
+function onStartBtnClick() {
+   refs.startBtn.disabled = true;
+   refs.stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+   }, delayValue);
+   
+};
+function onStopBtnClick() {
+   refs.startBtn.disabled = false;
+   refs.stopBtn.disabled = true;
+   clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
