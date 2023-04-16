d3fdad7661a5589ef1590df9ce1d75c20867e88e
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2023-04-16 22:09:44 +0300

    correct task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,11 +10,60 @@ const refs = {
    seconds: document.querySelector('[data-seconds]'),
 };
 
+
+const DELAY = 1000;
+let startTime = null;
+refs.startBtn.disabled = true;
+refs.startBtn.addEventListener('click', () => {
+   timer.start();
+})
+const timer = {
+   isActive: false, 
+   timerId:null,
+   start() {
+      if (this.isActive) {
+         return;
+      };
+      this.isActive = true;
+    
+   this.timerId =  setInterval(() => {
+         const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+         const timerEditor = convertMs(deltaTime);
+      const time = updateClockface(timerEditor);
+      if (deltaTime <= 0) {
+         this.stop();
+      }
+      },DELAY)
+   },
+   stop() {
+      clearInterval(this.timerId);
+      this.isActive = false;
+      timerEditor = convertMs(0);
+      time = updateClockface(timerEditor);
+   }
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+   onClose(selectedDates) {
+      // console.log(selectedDates[0]);
+      startTime = selectedDates[0];
+      // console.log(startTime)
+      if (selectedDates[0] < Date.now()) {
+          window.alert('please choose date in future');
+      };
+      refs.startBtn.disabled = false;
+   }
+};
 function updateClockface({ days, hours, minutes, seconds }) {
-    refs.days.textContent = `${days}`;
-    refs.hours.textContent = `${hours}`;
-    refs.minutes.textContent = `${minutes}`;
-    refs.seconds.textContent = `${seconds}`;
+   refs.days.textContent = ` ${ days }`;
+   refs.hours.textContent = `${ hours }`;
+   refs.minutes.textContent = `${minutes }`;
+   refs.seconds.textContent = `${ seconds }`;
 }
 function pad(value) {
     return String(value).padStart(2, '0');
@@ -38,58 +87,43 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-class Timer{
-   constructor({onTick}) {
-      this.intervalId = null;
-      this.isActive = false;
-      this.onTick = onTick;
-   }
-   start(startTime) {
-      if (this.isActive) {
-         return;
-      }
-      this.startTime = startTime;
-      this.isActive = true;
-     this.intervalId= setInterval(()=>{
-         const currentTime = Date.now();
-         
-         const deltaTime = startTime-currentTime;
-         
-        const time = convertMs(deltaTime);
-        this.onTick(time);
-        if (deltaTime <= 0) {
-         this.stop()
-      }
-      }, 1000)
-   }
-   stop() {
-      clearInterval(this.intervalId);
-      this.isActive = false;
-      const time = convertMs(0)
-      this.onTick(time)
-   }
-}
-const timer = new Timer({onTick:updateClockface})
-refs.startBtn.disabled = true;
-refs.startBtn.addEventListener('click', () => {
-   timer.start(startTime);
-})
-let startTime = null;
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-   onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    startTime =selectedDates[0]
-      if (selectedDates[0] < Date.now()) {
-         window.alert('please choose date in future');
-         
-      };
-      refs.startBtn.disabled = false;
-   }
-};
+
+
+
+
 
 
 flatpickr(refs.input, options);
+// class Timer{
+//    constructor({onTick}) {
+//       this.intervalId = null;
+//       this.isActive = false;
+//       this.onTick = onTick;
+//    }
+//    start(startTime) {
+//       if (this.isActive) {
+//          return;
+//       }
+//       this.startTime = startTime;
+//       this.isActive = true;
+//      this.intervalId= setInterval(()=>{
+//          const currentTime = Date.now();
+         
+//          const deltaTime = startTime-currentTime;
+         
+//         const time = convertMs(deltaTime);
+//         this.onTick(time);
+//         if (deltaTime <= 0) {
+//          this.stop()
+//       }
+//       }, 1000)
+//    }
+//    stop() {
+//       clearInterval(this.intervalId);
+//       this.isActive = false;
+//       const time = convertMs(0)
+//       this.onTick(time)
+//    }
+// }
+// const timer = new Timer({onTick:updateClockface})
+// refs.startBtn.disabled = true;
