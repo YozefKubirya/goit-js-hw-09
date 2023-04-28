a9712a6e9babae3fd89c9fc0c10d9c6fb26aa4a4
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2023-04-28 16:21:07 +0000

    Deploying to gh-pages from @ YozefKubirya/goit-js-hw-09@98268419b54a296745e3e29a1cfb8d0a475ffaee 🚀

---
+++ 01-color-switcher.8b459c79.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)}));var n=null;t.stopBtn.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.8b459c79.js.map
